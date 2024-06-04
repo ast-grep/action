fc1e041167c8ad540c81f2169e5675e7242b0e9d
Will Binns-Smith <wbinnssmith@gmail.com>
2024-06-04 10:51:30 -0700

    Add support for running the action on limited paths
    This adds an additional input, `paths`, corresponding to `PATHS` passed to the ast-grep cli, to limit the paths the action runs on.
    
    Test Plan: Extended the workflow to include an advanced testcase.

--- src/index.ts
+++ src/index.ts
@@ -8,10 +8,24 @@ async function main() {
   } else {
     await exec.exec('npm', ['install', '@ast-grep/cli', '--global'])
   }
+
   const config = core.getInput('config')
-  const args = config
-    ? ['scan', '-c', config, '--format', 'github']
-    : ['scan', '--format', 'github']
+  const paths = core.getInput('paths')
+  const args = ['scan']
+
+  if (config) {
+    args.push('-c', config)
+  }
+
+  args.push('--format', 'github')
+
+  if (paths) {
+    // GitHub actions doesn't support a YAML list, so use a string
+    // https://stackoverflow.com/questions/75420197/how-to-use-array-input-for-a-custom-github-actions
+    // and allow for escaped whitespace
+    args.push(...paths.split(/(?<!\\)\s+/))
+  }
+
   await exec.exec('ast-grep', args)
 }
 
